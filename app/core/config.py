43319d8d9d24b371dd97a3a963846b652ee0c523
UmerMalik298 <umer23672@gmail.com>
2026-03-03 14:52:30 +0500

    supabase

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,11 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     LIVEKIT_URL: str
     LIVEKIT_API_KEY: str
     LIVEKIT_API_SECRET: str
-    
+    SUPABASE_URL: str
+    SUPABASE_KEY: str
     class Config:
         env_file = ".env"
 
